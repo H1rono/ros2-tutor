f1274d8cc0fbaf0d8ba8b3ef667f235238833d0b
H1rono <hronok66@gmail.com>
2024-05-17 18:02:53 +0900

    Swap subscriber, subscriber_main

--- pubsub/src/subscriber.cpp
+++ pubsub/src/subscriber.cpp
@@ -1,12 +1,15 @@
-#include <memory>
+#include <functional>
+#include <string>
 
 #include "pubsub/subscriber.hpp"
-#include "rclcpp/rclcpp.hpp"
 
-int main(int argc, char* argv[]) {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<sub::Subscriber>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
+void sub::Subscriber::topic_callback(const std_msgs::msg::String& msg) {
+    RCLCPP_INFO(this->get_logger(), "hear %s", msg.data.c_str());
+}
+
+sub::Subscriber::Subscriber() : rclcpp::Node("subscriber"), subscription() {
+    using std::placeholders::_1;
+    auto callback = std::bind(&::sub::Subscriber::topic_callback, this, _1);
+    subscription
+        = this->create_subscription<std_msgs::msg::String>("chatter", 10, callback);
 }
