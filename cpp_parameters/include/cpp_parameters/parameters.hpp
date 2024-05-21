1dad026de3d03a36bbf30e6e59daff79f1e10f5a
H1rono <hronok66@gmail.com>
2024-05-22 05:49:16 +0900

    Fix format

--- cpp_parameters/include/cpp_parameters/parameters.hpp
+++ cpp_parameters/include/cpp_parameters/parameters.hpp
@@ -4,16 +4,16 @@
 
 namespace param {
 
-    class Parameters : public rclcpp::Node {
-    private:
-        rclcpp::TimerBase::SharedPtr timer;
-        std::shared_ptr<rclcpp::ParameterEventHandler> event_handler;
-        std::shared_ptr<rclcpp::ParameterCallbackHandle> callback_handle;
+class Parameters : public rclcpp::Node {
+private:
+    rclcpp::TimerBase::SharedPtr                     timer;
+    std::shared_ptr<rclcpp::ParameterEventHandler>   event_handler;
+    std::shared_ptr<rclcpp::ParameterCallbackHandle> callback_handle;
 
-        void timer_callback();
+    void timer_callback();
 
-    public:
-        explicit Parameters(const rclcpp::NodeOptions &options);
-    };
+public:
+    explicit Parameters(const rclcpp::NodeOptions& options);
+};
 
 }
--- cpp_parameters/src/parameters.cpp
+++ cpp_parameters/src/parameters.cpp
@@ -1,6 +1,6 @@
-#include <memory>
 #include <chrono>
 #include <functional>
+#include <memory>
 #include <string>
 
 #include "rclcpp_components/register_node_macro.hpp"
@@ -12,20 +12,24 @@ void param::Parameters::timer_callback() {
     RCLCPP_INFO_STREAM(this->get_logger(), "Hello, " << my_param << "!");
 }
 
-param::Parameters::Parameters(const rclcpp::NodeOptions &options) : rclcpp::Node("parameter", options), timer() {
+param::Parameters::Parameters(const rclcpp::NodeOptions& options) :
+    rclcpp::Node("parameter", options),
+    timer() {
     using namespace std::chrono_literals;
     this->declare_parameter("my_parameter", "world");
     this->event_handler = std::make_shared<rclcpp::ParameterEventHandler>(this);
-    auto callback = [this](const rclcpp::Parameter &p) {
+    auto callback       = [this](const rclcpp::Parameter& p) {
         RCLCPP_INFO_STREAM(
-                this->get_logger(),
-                "callback: Received an update to parameter \"" << p.get_name() << "\" of type " << p.get_type_name()
-                                                               << ": \"" << p.as_string() << "\""
+            this->get_logger(),
+            "callback: Received an update to parameter \""
+                << p.get_name() << "\" of type " << p.get_type_name() << ": \""
+                << p.as_string() << "\""
         );
     };
-    this->callback_handle = this->event_handler->add_parameter_callback("my_parameter", callback);
+    this->callback_handle
+        = this->event_handler->add_parameter_callback("my_parameter", callback);
     this->timer
-            = this->create_wall_timer(1s, std::bind(&Parameters::timer_callback, this));
+        = this->create_wall_timer(1s, std::bind(&Parameters::timer_callback, this));
 }
 
 RCLCPP_COMPONENTS_REGISTER_NODE(param::Parameters)
