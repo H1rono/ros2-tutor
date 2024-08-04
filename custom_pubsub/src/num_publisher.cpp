0dc43130aee7ab1c64a838be8f735f9a40fc686c
H1rono <hronok66@gmail.com>
2024-08-05 00:34:31 +0900

    Add initial_count in num_pub

--- custom_pubsub/src/num_publisher.cpp
+++ custom_pubsub/src/num_publisher.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <functional>
+#include <string>
 
 #include "rclcpp_components/register_node_macro.hpp"
 
@@ -11,9 +12,13 @@ num_pub::NumPublisher::NumPublisher(const rclcpp::NodeOptions& options) :
     timer(),
     count(0) {
     using namespace std::chrono_literals;
+    constexpr char parameter_name[] = "initial_count";
+
+    this->declare_parameter(parameter_name, 0);
     this->publisher = this->create_publisher<tutorial_interfaces::msg::Num>("num", 10);
     this->timer
         = this->create_wall_timer(500ms, std::bind(&NumPublisher::timer_callback, this));
+    this->count = static_cast<std::size_t>(this->get_parameter(parameter_name).as_int());
 }
 
 void num_pub::NumPublisher::timer_callback() {
